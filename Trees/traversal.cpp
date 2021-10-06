4cdd99b92cb2e4ad504d1b031b399a3ca66dafb5
DDOMohit <66627911+DDOMohit@users.noreply.github.com>
2021-10-06 21:47:22 +0530

    Create traversal.cpp

---
+++ Trees/traversal.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+/*******************preorder*******************/
+void preorder(struct Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+/******************inorder**********************/
+void inorder(struct Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+/*****************postorder********************/
+void postorder(struct Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+/*********************************************/
+int main()
+{
+    struct Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+    cout << "the preorder traversal is " << endl;
+    preorder(root);
+    cout<<endl;
+    cout << "the inorder traversal is " << endl;
+    inorder(root);
+    cout<<endl;
+    cout << "the postorder traversal is " << endl;
+    postorder(root);
+}
